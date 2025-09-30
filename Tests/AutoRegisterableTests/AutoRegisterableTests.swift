791756f76a325c895d090d0ae6296c81f8c848fd
patrickavs <patrick-rc-pfeil@gmx.de>
2025-09-30 09:43:15 +0200

    format using `swift format --recursive --in-place .`

--- Tests/AutoRegisterableTests/AutoRegisterableTests.swift
+++ Tests/AutoRegisterableTests/AutoRegisterableTests.swift
@@ -4,23 +4,23 @@ import SwiftSyntaxMacrosTestSupport
 import XCTest
 
 #if canImport(AutoRegisterableMacros)
-    import AutoRegisterableMacros
+  import AutoRegisterableMacros
 
-    final class AutoRegisterableTests: XCTestCase {
-        let testMacros: [String: Macro.Type] = [
-            "AutoRegisterable": AutoRegisterableMacro.self,
-        ]
+  final class AutoRegisterableTests: XCTestCase {
+    let testMacros: [String: Macro.Type] = [
+      "AutoRegisterable": AutoRegisterableMacro.self
+    ]
 
-        func testAutoRegisterableInAppService() throws {
-            testMacro(macros: testMacros)
-        }
-
-        func testAutoRegisterableInFMSServiceProduction() throws {
-            testMacro(macros: testMacros)
-        }
-
-        func testStructWithoutDependencyEntries() throws {
-            testMacro(macros: testMacros)
-        }
+    func testAutoRegisterableInAppService() throws {
+      testMacro(macros: testMacros)
     }
+
+    func testAutoRegisterableInFMSServiceProduction() throws {
+      testMacro(macros: testMacros)
+    }
+
+    func testStructWithoutDependencyEntries() throws {
+      testMacro(macros: testMacros)
+    }
+  }
 #endif
